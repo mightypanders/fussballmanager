d9981deffbc85d4c9a7f68d096da78170c20926f
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2016-07-07 09:53:58 +0200

    Erste Klasse

---
+++ manager/manager/Entities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manager
+{
+    class Entity
+    {
+        uID _ID;
+        public Entity(uID ID)
+        {
+            _ID = ID;
+        }
+    }
+    class Person : Entity
+    {
+        private string _name;
+        private DateTime _birth;
+        public Person(uID ID, string name, DateTime birth) : base(ID)
+        {
+            _name = name;
+            _birth = birth;
+
+        }
+    }
+    public struct uID
+    {
+
+    }
+}
