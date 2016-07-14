ace174c776e9c0959b20bb9169e853c03b812145
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2016-07-14 16:11:20 +0200

    Team, Game, Stadium

--- manager/manager/Entities.cs
+++ manager/manager/Entities.cs
@@ -25,6 +25,58 @@ namespace manager
 
         }
     }
+
+    class Team : Entity
+    {
+
+        private List<Person> _Members;
+        private string _name;
+        private DateTime _founded;
+        private Person _manager;
+        private Person _trainer;
+
+        public Team(uID ID, string name, DateTime founded, Person manager, Person trainer) : base(ID)
+        {
+            _name = name;
+            _founded = founded;
+            _manager = manager;
+            _trainer = trainer;
+        }
+
+    }
+
+
+    class Game : Entity
+    {
+        DateTime _matchdate;
+        string _location;
+        Team _receiving;
+        Team _guest;
+        public Game(uID ID, DateTime matchdate, string location, Team receiving, Team guest) : base(ID)
+        {
+            _matchdate = matchdate;
+            _location = location;
+            _receiving = receiving;
+            _guest = guest;
+        }
+    }
+
+    class Stadium : Entity
+    {
+        string _name;
+        string _city;
+        Team _belongsTo;
+
+        public Stadium(uID ID, string name, string city, Team belongsTo) : base(ID)
+        {
+            _name = name;
+            _city = city;
+            _belongsTo = belongsTo;
+
+        }
+    }
+
+
     public struct uID
     {
 
