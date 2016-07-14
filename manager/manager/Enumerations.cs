eeeb45249f269d9251971ba1235d037e7d714eb4
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2016-07-14 16:14:28 +0200

    Enums

---
+++ manager/manager/Enumerations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manager
+{
+    static class Enumerations
+    {
+
+        public enum Weather
+        {
+            Fair = 1,
+            Rain,
+            Sun,
+            Snow,
+            Hail,
+            Storm
+        }
+        public enum Satisfaction
+        {
+            Happy = 1,
+            Sad,
+            Angry,
+            Motivated,
+            Plotting,
+            Indifferent,
+            Corrupt
+        }
+    }
+}
